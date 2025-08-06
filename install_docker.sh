b9b262fa5e1bd17756d81a792591e381fea5d2e2
andrydtm <92445836+andrydtm@users.noreply.github.com>
2025-08-06 23:37:08 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -44,10 +44,8 @@ echo "========================="
 sudo docker run --rm hello-world
 
 echo "========================="
-echo "DOWNLOAD & INSTALL CMS"
+echo "DOWNLOAD CMS"
 echo "========================="
 
 sudo curl -o cms_install.sh https://cms.s.cdatayun.com/cms_linux/cms_install.sh
 sudo chmod +x ./cms_install.sh
-
-sudo ./cms_install.sh install --version 3.6.26
